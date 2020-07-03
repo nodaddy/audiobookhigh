2ce16095e3b7147cecdb9a63eea932ca4d24b3ac
NEELESH SHARMA <nsharma1@me.iitr.ac.in>
2020-07-03 16:34:15 +0530

    sfas

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,11 +7,11 @@ class Profile extends React.Component {
              <div className="container" align="center">
                  <div className="col-sm-12">
                      <br/>
-                    <h3>Neelesh Sharma</h3>
+                    <h4>Neelesh Sharma</h4>
                     Bachelor of Technology, <br/>
                     Production and Industrial Engineering(Mechanical and Industrial Department),<br/>
                     IIT Roorkee, Uttarakhand, India<br/><br/>
-                    <h4>Contact Details:</h4>
+                    <h5>Contact Details:</h5>
                     +91 9870938860 <br/>
                     <a href="https://www.linkedin.com/in/neelesh-sharma-6178971aa/">My linkedIn profile</a>
                  </div>
